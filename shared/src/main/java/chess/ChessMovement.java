438f04163ee80cd1041bf595b0d22ac2dc652b16
TagNakamoto <tahoshna@byu.edu>
2024-01-20 16:19:50 -0700

    Added ChessMovement file and a prelim switch statement to ChessPiece. Nothing big just saving before dinner

---
+++ shared/src/main/java/chess/ChessMovement.java
@@ -0,0 +1,9 @@
+package chess;
+
+//Make class of ChessMovement, create subclasses for each piece type, piecemoves method in this class
+public class ChessMovement {
+    
+}
+class Bishop extends ChessMovement {
+
+}
