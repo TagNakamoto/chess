817b893696e20d19592e346eacaea37f85c3fa12
TagNakamoto <tahoshna@byu.edu>
2024-02-21 17:00:38 -0700

    Got normal register and clear logic working. Next gotta troubleshoot the specific error messages and codes

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,11 +1,31 @@
 package service;
 
+import dataAccess.*;
 import model.AuthData;
 import model.UserData;
 
+import java.util.UUID;
+
 public class UserService {
-    public AuthData register(UserData user){
-        //Pretend like this actually works
-        return new AuthData("ugood", "taho");
+    private final UserDAO users = new MemoryUserDAO();
+    private final AuthDAO auths = new MemoryAuthDAO();
+    public UserService(){}
+    public AuthData register(UserData user) throws DataAccessException{
+        String username = user.username();
+        if(users.getUser(username) ==null){
+            users.insertUser(user);
+            AuthData tempAuth = createAuth(username);
+            auths.insertAuth(tempAuth);
+            return tempAuth;
+        }
+
+        return null;
+    }
+    public void clear(){
+        users.clear();
+        auths.clear();
+    }
+    private AuthData createAuth(String username){
+        return new AuthData(UUID.randomUUID().toString(), username);
     }
 }
