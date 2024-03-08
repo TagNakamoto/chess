bccc199855fd6d4ad520d81300b9cfd66bc6bac0
TagNakamoto <tahoshna@byu.edu>
2024-03-07 19:21:33 -0700

    Implemented ListGames tests

--- server/src/test/java/dataAccessTests/SQLGamesTests.java
+++ server/src/test/java/dataAccessTests/SQLGamesTests.java
@@ -60,6 +60,7 @@ public class SQLGamesTests {
             userDAO.insertUser(newObserver);
             int gameID = gameDAO.insertGame(gameName);
             gameDAO.addObserver(gameID, newObserver.username());
+            assertTrue(gameID > 0);
         }
         catch(DataAccessException ex){
             fail("Unexpected exception:" + ex.getMessage());
@@ -77,5 +78,62 @@ public class SQLGamesTests {
         catch(DataAccessException ex){
             fail("Unexpected exception:" + ex.getMessage());
         }
+    }
+
+    @Test
+    public void normalAddPlayer(){
+        UserDAO userDAO = new SQLUserDAO();
+        GameDAO gameDAO = new SQLGameDAO();
+        String gameName = "This is a game name";
+        try{
+            UserData newObserver = new UserData("observer", "password", "email");
+            userDAO.insertUser(newObserver);
+            int gameID = gameDAO.insertGame(gameName);
+            gameDAO.addPlayer("BLACK",gameID, newObserver.username());
+            assertThrows(DataAccessException.class,
+                    ()->gameDAO.addPlayer("BLACK", gameID, newObserver.username()));
+        }
+        catch(DataAccessException ex){
+            fail("Unexpected exception:" + ex.getMessage());
+        }
+    }
+
+    @Test
+    public void unAddedAddPlayer(){
+        GameDAO gameDAO = new SQLGameDAO();
+        String gameName = "This is a game name";
+        try {
+            int gameID = gameDAO.insertGame(gameName);
+            assertThrows(DataAccessException.class,
+                    () -> gameDAO.addPlayer("BLACK", gameID, null));
+        }
+        catch(DataAccessException ex){
+            fail("Unexpected exception:" + ex.getMessage());
+        }
+    }
+
+    @Test
+    public void normalListGamesTest(){
+        GameDAO gameDAO = new SQLGameDAO();
+        String gameName = "This is a game name";
+        try {
+            int gameID = gameDAO.insertGame(gameName);
+            assertEquals(gameID, gameDAO.listGames().stream().toList().get(0).gameID());
+        }
+        catch(DataAccessException ex){
+            fail("Unexpected exception:" + ex.getMessage());
+        }
+    }
+
+    @Test
+    public void emptyGamesListGamesTest(){
+        GameDAO gameDAO = new SQLGameDAO();
+        try {
+            assertTrue(gameDAO.listGames().isEmpty());
+        }
+        catch(DataAccessException ex){
+            fail("Unexpected exception:" + ex.getMessage());
+        }
+    }
     }
 }
