b8f785e263a3ce91f792bfea00870b3865adfd45
TagNakamoto <tahoshna@byu.edu>
2024-03-07 17:28:03 -0700

    Implemented insert user tests

--- server/src/test/java/dataAccessTests/SQLUserTests.java
+++ server/src/test/java/dataAccessTests/SQLUserTests.java
@@ -1,12 +1,19 @@
 package dataAccessTests;
 
 import dataAccess.*;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SQLUserTests {
+    @BeforeEach
+    public void setup(){
+        clearTest();
+    }
+
     @Test
     public void clearTest() {
         UserDAO userDAO = new SQLUserDAO();
@@ -21,6 +28,33 @@ public class SQLUserTests {
         catch(DataAccessException ex){
             fail("Unexpected exception:" + ex.getMessage());
         }
+    }
+
+    @Test
+    public void normalInsertUserTest(){
+        UserDAO userDAO = new SQLUserDAO();
+        try{
+            UserData newUser = new UserData("Normal user", "password", "email");
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+            userDAO.insertUser(newUser);
+            UserData retrievedUser = userDAO.getUser(newUser.username());
+
+            assertEquals(newUser.username(), retrievedUser.username());
+            assertTrue(encoder.matches(newUser.password(), retrievedUser.password()));
+        }
+        catch(DataAccessException ex){
+            fail("Unexpected exception:" + ex.getMessage());
+        }
+    }
+
+    @Test
+    public void noPasswordInsertUserTest(){
+        UserDAO userDAO = new SQLUserDAO();
+
+        UserData newUser = new UserData("user no password", null, "email");
+
+        assertThrows(DataAccessException.class, ()-> userDAO.insertUser(newUser));
 
     }
 }
