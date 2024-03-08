29a808f1eae27a996ce57e1e34e55a4d788747d4
TagNakamoto <tahoshna@byu.edu>
2024-03-07 17:13:38 -0700

    clear test implemented

--- server/src/test/java/dataAccessTests/SQLUserTests.java
+++ server/src/test/java/dataAccessTests/SQLUserTests.java
@@ -1,18 +1,22 @@
 package dataAccessTests;
 
-import dataAccess.DataAccessException;
-import dataAccess.SQLUserDAO;
-import dataAccess.UserDAO;
+import dataAccess.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class SQLUserTests {
     @Test
     public void clearTest() {
         UserDAO userDAO = new SQLUserDAO();
+        AuthDAO authDAO = new SQLAuthDAO();
+        GameDAO gameDAO = new SQLGameDAO();
         try{
+            gameDAO.clear();
+            authDAO.clear();
             userDAO.clear();
+            assertTrue(userDAO.isEmpty());
         }
         catch(DataAccessException ex){
             fail("Unexpected exception:" + ex.getMessage());
